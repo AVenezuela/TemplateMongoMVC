c0a9972cd1cee8ab734b3f6824dbc73bf5dbd56e
avaraujo <avaraujo@NBBV022082>
2017-01-13 19:37:12 -0200

    ui.grid upgrade employee changes

--- TemplateMongo.SPA/Scripts/modules/employee.js
+++ TemplateMongo.SPA/Scripts/modules/employee.js
@@ -1,7 +1,7 @@
 ﻿var employee = angular.module('mdlEmployee', []);
 employee.controller('EmployeeCtrl', ['$scope', '$state', 'employeeModel', 'hotkeys', function ($scope, $state, model, hotkeys) {
     $scope.model = model;
-    $scope.initial = angular.copy(model);
+    $scope.initial = angular.copy(model);    
 
     hotkeys.add({
         combo: 'n+f',
@@ -9,39 +9,60 @@ employee.controller('EmployeeCtrl', ['$scope', '$state', 'employeeModel', 'hotke
         callback: function () {
             $state.go('employee.new')
         }
-    });
+    });    
 
     $scope.dtOptions = {
         data: model.Employees
         , paginationPageSizes: [10, 20, 30]
         , paginationPageSize: 10
+        , enableRowSelection: true
+        , multiSelect: false
+        , enableRowHeaderSelection: false
+        , enableHidingColumn: false
+        , enableFullRowSelection: false
+        , enableHorizontalScrollbar: 0
+        , enableVerticalScrollbar: 0
         , columnDefs: [
-           { name: 'Nome', field: 'Name' }
-           , { name: 'Nascimento', field: 'BirthDate' }
-           , { name: 'Telefone', field: 'PhonePrincipal' }
+             { name: 'Nome', field: 'Name' }
+           , { name: 'Nascimento', field: 'BirthDate', maxWidth: 110, minWidth: 110 }
+           , { name: 'Telefone', field: 'PhonePrincipal', maxWidth: 120, minWidth: 120 }
            , { name: 'E-mail', field: 'Email' }
+           , { name: '#', maxWidth: 40, minWidth: 40, cellTemplate: '<div class="ui-grid-cell-contents"><i class="fa fa-edit action-ico" ng-click="grid.appScope.rowSelect(row, rowRenderIndex)"></i></div>', enableSorting: false, enableHiding: false }
         ]
     }
+
+    $scope.rowSelect = function (row, index) {
+        $scope.gridApi.selection.selectRow(row.entity);
+        $scope.model.EmployeeBag = angular.copy(row.entity)
+        $state.go('employee.edit', { employeeId:row.entity.MongoID })
+    }
+
+    $scope.dtOptions.onRegisterApi = function (gridApi) {
+        $scope.gridApi = gridApi;
+    };
 
     $scope.$on("$destroy", function () {
         $scope.model.EmployeeBag = $scope.initial.EmployeeBag
     });
 }]),
-actionEmployee = function ($scope, $http, $stateParams, $state, model, EmployeeService) {
+actionEmployee = function ($scope, $http, $stateParams, $state, model, EmployeeService, uiGridConstants) {
     $scope.submitForm = function () {
         EmployeeService.actionEmployee($scope.model.EmployeeBag).then(
         function (response) {
-            switch($state.$current.name) {
+            switch ($state.$current.name) {
                 case 'employee.new':
-                    model.Employees.push(response.data)                    
+                    model.Employees.push(response.data)
                     break;
                 case 'employee.edit':
-                    model.Employees.splice($stateParams.index, 1, response.data)
+                    var entity = $scope.gridApi.grid.selection.lastSelectedRow.entity
+                    $scope.gridApi.grid.selection.lastSelectedRow.entity = angular.extend(entity, response.data);
+                    //model.Employees.splice($stateParams.index, 1, entity)
                     break;
-                default:                   
+                default:
                     break;
-            } 
+            }
 
+            $scope.gridApi.selection.clearSelectedRows();
             model.EmployeeBag = $scope.model.EmployeeBag = {}
             $scope.successNonBlockMessage('Funcionário "' + response.data.Name + '" salvo com sucesso.')
             $scope.apply()
@@ -53,8 +74,9 @@ actionEmployee = function ($scope, $http, $stateParams, $state, model, EmployeeS
 
     $scope.cancelForm = function () {
         model.EmployeeBag = $scope.model.EmployeeBag = angular.copy($scope.initial.EmployeeBag);
-        $state.go('employee')
-    }
+        $scope.gridApi.selection.clearSelectedRows();
+        $state.go('employee')        
+    }    
 },
 employee.controller('NewEmployeeCtrl', [
         '$scope',
@@ -63,22 +85,21 @@ employee.controller('NewEmployeeCtrl', [
         '$state',
         'employeeModel',
         'EmployeeService',
-        function ($scope, $http, $stateParams, $state, model, EmployeeService) {
-            $scope.model.EmployeeBag = $scope.initial.EmployeeBag
-            actionEmployee($scope, $http, $stateParams, $state, model, EmployeeService)
+        'uiGridConstants',
+        function ($scope, $http, $stateParams, $state, model, EmployeeService, uiGridConstants) {            
+            actionEmployee($scope, $http, $stateParams, $state, model, EmployeeService, uiGridConstants)
         }
 ]),
 employee.controller('EditEmployeeCtrl', [
         '$scope',
         '$http',
         '$stateParams',
-        '$state',
+        '$state',        
         'employeeModel',
         'EmployeeService',
-        'selected',
-        function ($scope, $http, $stateParams, $state, model, EmployeeService, selected) {
-            $scope.model.EmployeeBag = selected.EmployeeBag
-            actionEmployee($scope, $http, $stateParams, $state, model, EmployeeService)
+        'uiGridConstants',
+        function ($scope, $http, $stateParams, $state, model, EmployeeService, uiGridConstants) {            
+            actionEmployee($scope, $http, $stateParams, $state, model, EmployeeService, uiGridConstants)            
         }
 ]),
 employee.service('EmployeeService', ['$http', 'apiConfig', '$q', function ($http, apiConfig, $q) {
@@ -125,15 +146,7 @@ employee.config(['$stateProvider',
                 , templateUrl: '/Employee/Action',
                 controller: 'NewEmployeeCtrl'
             }).state('employee.edit', {
-                url: '/edit/{employeeId}?index',
-                resolve: {
-                    selected: ['EmployeeService', '$stateParams', function (EmployeeService, $stateParams) {
-                        return EmployeeService.getEmployee($stateParams.employeeId);
-                    }]
-                    /*, lazyLoad: function ($ocLazyLoad) {
-                        return $ocLazyLoad.load('lzyDataTable');
-                    }*/
-                },
+                url: '/edit/{employeeId}',
                 templateUrl: '/Employee/Action/',
                 controller: 'EditEmployeeCtrl'
             })
